9344bf67acbacb604393bcfa071ac06c6166b804
SatyanandYadav <satyanandyadav1517@gmail.com>
2024-07-19 17:18:35 +0530

    Add test case to find th sum of all numbers and change function name

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-export const stringCalculator = (numbers) => {
+export const add = (numbers) => {
   if (!numbers) return 0;
   const sum = numbers
     .split(",")
--- src/tests/index.test.js
+++ src/tests/index.test.js
@@ -1,16 +1,20 @@
 import { describe, it, expect } from "vitest";
-import { stringCalculator } from "..";
+import { add } from "..";
 
 describe("string calculation", () => {
   it("should return zero if it is empty string", () => {
-    expect(stringCalculator("")).toBe(0);
+    expect(add("")).toBe(0);
   });
 
   it("should return first number if it is contains one number string", () => {
-    expect(stringCalculator("5")).toBe(5);
+    expect(add("5")).toBe(5);
   });
 
   it("should return sum of first two number if a string contains two number separated by ','", () => {
-    expect(stringCalculator("5,3")).toBe(8);
+    expect(add("5,3")).toBe(8);
+  });
+
+  it("should return sum of all numbers if a string contains numbers separated by ','", () => {
+    expect(add("5,3,14,23")).toBe(45);
   });
 });
